4be807463b35b39e2b342ff724b5e0b05aaae91b
Desponark <129955348+Desponark@users.noreply.github.com>
2024-04-25 17:34:13 +0200

    add new physics layers for perception

---
+++ Nodes/GameplayStats.cs
@@ -0,0 +1,19 @@
+using Godot;
+
+
+[GlobalClass]
+public partial class GameplayStats : Node
+{
+	// gameplay stats node (general gameplay variables)
+	// - health, stamina
+	// - flight value
+	// - fight value
+	// - alert level
+	// - disposition
+	public float Flight;
+	public float MaxFlight = 100;
+	public float Fight;
+	public float MaxFight = 100;
+	public float Alert;
+	public float MaxAlert = 100;
+}

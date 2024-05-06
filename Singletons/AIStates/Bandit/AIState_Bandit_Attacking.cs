fe3ba84e66962de7dcf323cae4761459223ef575
Desponark <129955348+Desponark@users.noreply.github.com>
2024-05-06 18:26:16 +0200

    add attacking state

---
+++ Singletons/AIStates/Bandit/AIState_Bandit_Attacking.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Diagnostics;
+using System.Linq;
+using Godot;
+
+public class AIState_Bandit_Attacking : State<Cognition>
+{
+	private static readonly Lazy<AIState_Bandit_Attacking> lazy = new(() => new AIState_Bandit_Attacking());
+	public static AIState_Bandit_Attacking Instance { get { return lazy.Value; }}
+
+
+	public override void Enter(Cognition entity)
+	{
+		Debug.WriteLine("Enter attacking");
+
+		var player = (Vehicle)entity.Memory["lastSeenPlayer"];
+		
+		var seek = new Seek(player);
+		entity.Steering.Behaviours.Add(seek);
+
+		entity.Memory["attackDuration"] = 2f;
+	}
+
+	public override void Execute(Cognition entity, double delta)
+	{
+		var player = (Vehicle)entity.Memory["lastSeenPlayer"];
+
+		if (player == null)
+			entity.StateMachine.ChangeState(AIState_Bandit_Fighting.Instance);
+
+		// try attacking for a maximum amount of time only!
+		if ((float)entity.Memory["attackDuration"] < 0f)
+		{
+			entity.StateMachine.ChangeState(AIState_Bandit_Fighting.Instance);
+		}
+		entity.Memory["attackDuration"] = (float)entity.Memory["attackDuration"] - (float)delta;
+
+		// if we get close enough it counts as successfull attack for now
+		if (entity.Vehicle.Position.DistanceTo(player.Position) <= 1.5)
+		{
+			GD.Print("ATTACKED: " + player);
+			entity.StateMachine.ChangeState(AIState_Bandit_Fighting.Instance);
+		}
+	}
+
+	public override void Exit(Cognition entity)
+	{
+		Debug.WriteLine("Exit attacking");
+
+		var seek = entity.Steering.Behaviours.OfType<Seek>().FirstOrDefault();
+		entity.Steering.Behaviours.Remove(seek);
+	}
+}
--- Singletons/AIStates/Bandit/AIState_Bandit_Fighting.cs
+++ Singletons/AIStates/Bandit/AIState_Bandit_Fighting.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.Linq;
+using Godot;
 
 public class AIState_Bandit_Fighting : State<Cognition>
 {
@@ -12,16 +13,18 @@ public class AIState_Bandit_Fighting : State<Cognition>
 	{
 		Debug.WriteLine("Enter fighting");
 		
-		var vehicle = (Vehicle)entity.Memory["lastSeenPlayer"];
+		var player = (Vehicle)entity.Memory["lastSeenPlayer"];
 
-		var flee = new Flee(vehicle, 4);
+		var flee = new Flee(player, 4);
 		entity.Steering.Behaviours.Add(flee);
 
-		var approach = new Approach(vehicle);
+		var approach = new Approach(player, 6);
 		entity.Steering.Behaviours.Add(approach);
 
-		var orbit = new Orbit(vehicle, Random.Shared.NextDouble() >= 0.5);
+		var orbit = new Orbit(player, Random.Shared.NextDouble() >= 0.5);
 		entity.Steering.Behaviours.Add(orbit);
+
+		entity.Memory["attackInterval"] = (float)Random.Shared.Next(2, 6);
 	}
 
 	public override void Execute(Cognition entity, double delta)
@@ -31,15 +34,21 @@ public class AIState_Bandit_Fighting : State<Cognition>
 		{
 			entity.StateMachine.ChangeState(AIState_Bandit_Patrolling.Instance);
 			return;
-		} else
+		}
+		
+		if (entity.Vehicle.Position.DistanceTo(player.Position) > 15)
 		{
-			if (entity.Vehicle.Position.DistanceTo(player.Position) > 10)
-			{
-				entity.StateMachine.ChangeState(AIState_Bandit_Patrolling.Instance);
-			}
+			entity.StateMachine.ChangeState(AIState_Bandit_Patrolling.Instance);
 		}
 
-		// TODO: randomly attack
+		if (entity.Vehicle.Position.DistanceTo(player.Position) < 6)
+		{
+			if ((float)entity.Memory["attackInterval"] < 0)
+			{
+				entity.StateMachine.ChangeState(AIState_Bandit_Attacking.Instance);
+			}
+			entity.Memory["attackInterval"] = (float)entity.Memory["attackInterval"] - (float)delta;
+		}
 	}
 
 	public override void Exit(Cognition entity)

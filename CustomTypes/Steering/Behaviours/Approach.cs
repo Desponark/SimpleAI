2bf11088636a4f7bd95063c77e16334a6d435c54
Desponark <129955348+Desponark@users.noreply.github.com>
2024-05-06 14:28:52 +0200

    add new steering behaviours

---
+++ CustomTypes/Steering/Behaviours/Approach.cs
@@ -0,0 +1,34 @@
+using Godot;
+
+/// <summary>
+/// Returns a steering force that steers an agent towards the target position up to a certain distance from it
+/// </summary>
+public class Approach : SteeringBehaviour
+{
+	private Vector3 targetPos = Vector3.Zero;
+	private Vehicle targetVehicle = null;
+
+	private float distance = 6;
+
+	public Approach(Vector3 targetPos, float distance = 6)
+	{
+		this.targetPos = targetPos;
+	}
+
+	public Approach(Vehicle targetVehicle, float distance = 6)
+	{
+		this.targetVehicle = targetVehicle;
+	}
+
+	public override Vector3 Calculate(Vehicle vehicle, double delta)
+	{
+		var pos = targetVehicle != null ? targetVehicle.Position : targetPos;
+
+		if (vehicle.Position.DistanceTo(pos) < distance)
+		{
+			return Vector3.Zero;
+		}
+
+		return Seek.Calc(vehicle.Position, pos, vehicle.MaxSpeed);
+	}
+}
